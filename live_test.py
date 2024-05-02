7a0c9ea7a94f06b6d5214d8bee142e47b2414116
Dan Dumitriu <dan.dumitriu@emag.ro>
2024-05-02 20:25:48 +0300

    added screenshots

--- live_test.py
+++ live_test.py
@@ -1,23 +1,33 @@
 import requests
+import logging
 
+
+logging.basicConfig(level=logging.DEBUG)
 BASE_URI = 'https://predict-salary-w3jd.onrender.com'
+INFERENCE_URI = BASE_URI + "/"
 
-response = requests.post(BASE_URI + "/", json={
-        "age": 24,
-        "workclass": "Private",
-        "fnlgt": 284582,
-        "education": "Bachelors",
-        "education-num": 13,
-        "marital-status": "Never-married",
-        "occupation": "Adm-clerical",
-        "relationship": "Not-in-family",
-        "race": "White",
-        "sex": "Male",
-        "capital-gain": 0,
-        "capital-loss": 0,
-        "hours-per-week": 40,
-        "native-country": "United-States"
-    })
 
-assert response.status_code == 200
-assert response.json() == {"prediction": 0}
+data_input = {
+    "age": 24,
+    "workclass": "Private",
+    "fnlgt": 284582,
+    "education": "Bachelors",
+    "education-num": 13,
+    "marital-status": "Never-married",
+    "occupation": "Adm-clerical",
+    "relationship": "Not-in-family",
+    "race": "White",
+    "sex": "Male",
+    "capital-gain": 0,
+    "capital-loss": 0,
+    "hours-per-week": 40,
+    "native-country": "United-States"
+}
+
+response = requests.post(INFERENCE_URI, json=data_input)
+prediction = response.json()['prediction']
+
+print(f'POST {INFERENCE_URI}')
+print(f'Input {data_input}')
+print(f'Response status code: {response.status_code}')
+print(f'Inference result {prediction}')

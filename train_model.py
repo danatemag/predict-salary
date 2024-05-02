14c9810d27088e5ae8e009109ae89d7842463c0a
Dan Dumitriu <dan.dumitriu@emag.ro>
2024-05-02 09:28:56 +0300

    gh actions

--- train_model.py
+++ train_model.py
@@ -1,6 +1,7 @@
 # Script to train machine learning model.
 import os
 import json
+import logging
 import pickle
 import pandas as pd
 from sklearn.model_selection import train_test_split
@@ -8,19 +9,20 @@ from ml.data import process_data
 from ml.model import (train_model, compute_model_metrics,
                       compute_model_metrics_by_cat)
 
-# Add the necessary imports for the starter code.
+
+logging.basicConfig(level=logging.DEBUG)
 
 # Add code to load in the data.
-print('Loading data ...')
+logging.debug('Loading data ...')
 cur_dir = os.path.dirname(os.path.realpath(__file__))
 data = pd.read_csv(cur_dir + '/data/census.csv')
 
-print('Splitting data ...')
+logging.debug('Splitting data ...')
 # Optional enhancement, use K-fold cross validation instead of a
 # train-test split.
 train, test = train_test_split(data, test_size=0.20, random_state=42)
 
-print('Processing data ...')
+logging.debug('Processing data ...')
 
 cat_features = [
     "workclass",
@@ -42,22 +44,22 @@ X_test, y_test, _, _ = process_data(
 )
 
 # # Train and save a model.
-print('Training model ...')
+logging.debug('Training model ...')
 model = train_model(X_train, y_train)
 
-print('Predicting test data ...')
+logging.debug('Predicting test data ...')
 y_preds = model.predict(X_test)
 
-print('Generating metrics ...')
+logging.debug('Generating metrics ...')
 metrics = compute_model_metrics(y_test, y_preds)
 
-print('General metrics:')
+logging.debug('General metrics:')
 print(metrics)
 
 cat_metrics = compute_model_metrics_by_cat(
     test, y_test, y_preds, cat_features[0])
 
-with open(f'{cur_dir}/model/sliced_output.txt', 'w') as f:
+with open(f'{cur_dir}/slice_output.txt', 'w') as f:
     f.write(json.dumps(cat_metrics, indent=4))
 
 save_files = {
@@ -66,7 +68,7 @@ save_files = {
     'lb': lb
 }
 
-print('Saving model files ...')
+logging.debug('Saving model files ...')
 for key in save_files.keys():
     with open(f'{cur_dir}/model/{key}.pkl', 'wb') as f:
         pickle.dump(save_files[key], f)
